bbce378bf94eb6b8cca94267b6c8790389e87af1
Saidur Rahman Siam <64358153+Saltys-Git@users.noreply.github.com>
2024-05-03 18:49:20 +0600

    Update Hero.tsx
    hero card moved from navbar to hero component

--- src/app/(client-panel)/Hero.tsx
+++ src/app/(client-panel)/Hero.tsx
@@ -15,8 +15,10 @@ const Hero = ({bgSrc, children}:{bgSrc:string, children: React.ReactNode,}) => {
         alt="Background Image"
       />
       <div className="absolute inset-0 bg-black bg-opacity-70">
-        <NavbarComponent />
-        {children}
+          <div className="border-2 md:border-8 border-[#C6C71D] rounded-md md:rounded-2xl h-96 md:h-[760px] m-4 sm:m-24 relative bg-[#0e0c0b] bg-opacity-50 text-white p-0 md:px-10">
+              <NavbarComponent/>
+              {children}
+          </div>
       </div>
     </div>
   );
