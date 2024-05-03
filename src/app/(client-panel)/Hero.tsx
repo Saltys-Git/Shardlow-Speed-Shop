2fe228e98209477a586b45cf1f695cdf18559a71
Saidur Rahman Siam <64358153+Saltys-Git@users.noreply.github.com>
2024-05-03 18:39:13 +0600

    Update Hero.tsx

--- src/app/(client-panel)/Hero.tsx
+++ src/app/(client-panel)/Hero.tsx
@@ -4,11 +4,11 @@ import React from "react";
 import Image from "next/image";
 import NavbarComponent from "./Navbar";
 
-const Hero = (//get image props, child components ) => {
+const Hero = ({bgSrc, children}:{bgSrc:string, children: React.ReactNode,}) => {
   return (
     <div className="relative w-full min-h-[430px] md:min-h-dvh lg:min-h-lvh ">
       <Image
-        src="/bg.jpg"
+        src={bgSrc}
         layout="fill"
         objectFit="cover"
         objectPosition="center"
@@ -16,8 +16,8 @@ const Hero = (//get image props, child components ) => {
       />
       <div className="absolute inset-0 bg-black bg-opacity-70">
         <NavbarComponent />
+        {children}
       </div>
-      <div className="absolute inset-0 flex justify-center items-center"></div>
     </div>
   );
 };
