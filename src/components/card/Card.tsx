efe05e3cc4d80880b6c0a9997fb22c1593b81b06
opurbo007 <opurboujjol@gmail.com>
2024-05-06 13:19:32 +0600

    animated card

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -10,18 +10,25 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ image, title, description }) => {
   return (
-    <div className="py-10">
-      <div className="card">
-        <img src={image} alt="" className="border-b-[12px] border-[#eaeb88]" />
+    <div className="py-10 flex justify-center">
+      <div className="card ">
+        <img
+          src={image}
+          alt=""
+          id="img"
+          className="border-b-[12px] border-[#eaeb88]"
+        />
         <div className="info">
-          <div className="flex-col justify-center items-center text-black px-8">
+          <div className="flex-col justify-center items-center text-black px-8 text-center">
             <div className="flex justify-center items-center text-[32px]">
-              <h1 className="py-3 font-medium  italic capitalize">{title}</h1>
+              <h1 className="py-3 text-[25px] sm:text-[32px] sm:font-medium  italic capitalize">
+                {title}
+              </h1>
               <button>
                 <ChevronRight className="text-[#c3c53a]" size={45} />
               </button>
             </div>
-            <p>{description}</p>
+            <p className="text-[11px]">{description}</p>
           </div>
         </div>
       </div>
