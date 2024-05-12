f0d6eef6a612a9111630f0224973e3b6e115f46d
Saidur Rahman Siam <64358153+Saltys-Git@users.noreply.github.com>
2024-05-13 02:05:05 +0600

    Update FAQSection.tsx

--- src/components/services/FAQSection.tsx
+++ src/components/services/FAQSection.tsx
@@ -30,14 +30,14 @@ const FAQSection = () => {
       </h1>
       <p className="sm:text-[26px] text-[12px] pb-5 text-default-300 font-light tracking-wider text-balance text-center ">
         Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever
         since the 1500s, when an unknown printer took a galley of type and
         scrambled it to make a type specimen bookLorem Ipsum is simply dummy
         text of the printing and typesetting industry. Lorem Ipsum has been the
-        industry's standard dummy text ever since the 1500s, when an unknown
+        industry&apos;s standard dummy text ever since the 1500s, when an unknown
         printer took a galley of type and scrambled it to make a type specimen
         book.Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever
         since the 1500s, when an unknown printer took a galley of type and
         scrambled it to make a type specimen book.Lorem Ipsum is simply dummy
       </p>
